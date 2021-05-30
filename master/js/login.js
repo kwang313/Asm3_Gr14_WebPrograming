9527f5c72dcf4b3abd5041b8ce8b881c066eba03
Lee-Gain <s3878170@rmit.edu.vn>
2021-05-30 10:50:37 +0700

    Update login.js

--- master/js/login.js
+++ master/js/login.js
@@ -5,13 +5,6 @@ function loginValidation() {
     let errMsg = {
         olEmail: [],
     }
-    let userID = document.querySelector('#uId').value;
-    let resultEmail = validateEmail(userID);
-
-    if (resultEmail.length > 0) {
-        errMsg.olEmail.push(resultEmail[0]);
-    }
-
     displayValidation(errMsg);
     if (checkAllValidation(errMsg)){
         localStorage.userCurrentId = userID;
